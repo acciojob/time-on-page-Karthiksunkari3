d4f80d5c301c5e7d86a1d9089f0547f42a8ef182
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2023-05-30 22:33:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    // Function to update the seconds count every second
+    const timer = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+
+    // Cleanup function to stop the timer when the component is unmounted
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1> You are been on this page for {seconds} seconds</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
